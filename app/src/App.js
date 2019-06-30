20050dcdabf0226ac976583e3d9c354919915880
DgurJIupyf <Dgurjiupyff@mail.ru>
2019-07-01 00:21:01 +0300

    Баг после последнего автобуса, хз чё с селектом

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Clock2, SuuuuuperTest, ComboBox, ComboBox2, DivCenter} from './TestLab.js'
+import {Clock2, SuuuuuperTest, DivCenter} from './TestLab.js'
 
 function App() {
   return (
--- app/src/TestLab.js
+++ app/src/TestLab.js
@@ -6,14 +6,14 @@ const TimeToBus2 = [
   "from": "Иваново",
   "to": "Палех",
   "times": ["00:00", "00:01", "00:59", "01:00", "01:01", "01:59", "02:00", "02:01",
-  "02:59", "03:00", "03:01", "03:59"]
+  "02:59", "03:00", "03:01", "03:59", "17:30", "19:00", "19:30", "19:50"]
   }, 
   {
-    "from": "Palekh",
-    "to": "Ivanovo",
-    "times": ["13:40", "07:59", "08:00", "08:01", "08:59", "09:00", "09:01", "09:59", 
+    "from": "Палех",
+    "to": "Иваново",
+    "times": ["07:59", "08:00", "08:01", "08:59", "09:00", "09:01", "09:59", 
     "10:00", "10:01", "10:59", "11:00", "11:01", "11:59", "12:00", "12:01", "12:59", "13:00", "13:01",
-    "13:59", "14:00", "14:01", "14:59", "15:00"]
+    "13:59", "14:00", "14:01", "14:59", "15:00", "18:00"]
   },
   {
     "from": "Шуя",
@@ -84,15 +84,6 @@ function TimeSecond(currentDate, interval) {
     }  
     return date;
 }
-/*function TimerTop(interval) {
-    var data = setInterval(() => {
-        clearInterval(data);
-        data = 0
-    }, interval)
-    clearInterval(data)
-    data = 0
-    return (<label> {TimeCalc(RealTime(), TimeToBus2[0].times)} </label>)
-}*/
 
 export function Clock2() {
   const date = TimeSecond(clock(), 1000)
@@ -104,10 +95,10 @@ export function Clock2() {
 export function GetDateToBus (RealT, TToBus) { 
   function RealNumberMinuts () {
     const RealMinArr = parseIntArray(RealT.split(":"))
-    const RealMin = RealMinArr[0]*60 + RealMinArr[1] 
+    const RealMin = RealMinArr[0]*60 + RealMinArr[1]
     return RealMin
   }
-  function ToBusNumberMinuts () {
+  function ToBusNumberMinuts () { 
     const ToBusNumArr = TToBus.map(item => {
       const OnlyNum = parseIntArray(item.split(":"))
       const OnlySumNum = OnlyNum[0]*60 + OnlyNum[1]
@@ -116,7 +107,9 @@ export function GetDateToBus (RealT, TToBus) {
     return ToBusNumArr
   }
   const NearTimeToBus = ToBusNumberMinuts().indexOf(ToBusNumberMinuts().find(item => item > RealNumberMinuts ()))
-  if (TToBus[NearTimeToBus+1] && TToBus[NearTimeToBus+2]) 
+  if (TToBus[NearTimeToBus] === undefined)
+    return [TToBus[0],TToBus[1],TToBus[2]]
+  else if (TToBus[NearTimeToBus+1] && TToBus[NearTimeToBus+2]) 
     return [TToBus[NearTimeToBus],TToBus[NearTimeToBus+1],TToBus[NearTimeToBus+2]]
   else if (TToBus[NearTimeToBus+1] && TToBus[NearTimeToBus+2] === undefined)
     return [TToBus[NearTimeToBus],TToBus[NearTimeToBus+1],TToBus[0]]
@@ -125,39 +118,47 @@ export function GetDateToBus (RealT, TToBus) {
 }
 
 export function DivCenter () {
-  console.log(ComboBox())
-  return (<div> {ComboBox()}<label id='from' className="Strelka"> -> </label> {ComboBox2()} </div>)
-}
-
-export function SuuuuuperTest() {
-  const bus = TimeToBus2.filter(item => item.from === "Иваново" && item.to === "Палех")[0]
-  
-  return (<div>
-    1. Ближайший рейс через {TimeCalc(RealTime(), bus.times[0])} минут в {bus.times[0]} <br></br>
-    2. Рейс через {TimeCalc(RealTime(), bus.times[1])} минут в {bus.times[1]}<br></br>
-    3. Рейс через {TimeCalc(RealTime(), bus.times[2])} минут в {bus.times[2]}<br></br>
-  </div>
-  )
+  return (<div> {ComboBox()}<label id='from'> -> </label> {ComboBox2()} </div>)
 }
 
 export function ComboBox() {
   return (
-    <select id='from' className='select-css'>
+    <select id='from' className='select-css' onChange={function OnSelectionChange (select) {
+        //var selectedOption = select.options[select.selectedIndex];
+        alert ("The selected option is ");
+        console.log(select)
+    }
+    }>
       <option value='1'> Иваново </option>
       <option value='2'> Палех </option>
       <option value='3'> Шуя </option>
-      <option value='4'> Владимир </option>
     </select>   
   )
 }
 
 export function ComboBox2() {
   return (
-    <select id='from' className='select-css'>
+    <select id='to' className='select-css'>
       <option value='1'> Иваново </option>
       <option value='2'> Палех </option>
       <option value='3'> Шуя </option>
-      <option value='4'> Владимир </option>
     </select>   
+  )
+}
+
+export function SuuuuuperTest() {  
+  const IndexValue1 = ComboBox2("Иваново")
+  //const IndexValue2 = ComboBox2().options.selectedIndex
+  //const text1 = ComboBox().options[IndexValue1].text
+  //const text2 = ComboBox2().options[IndexValue2].text
+  //console.log(IndexValue1/*, IndexValue2, text1, text2*/)
+  const bus = TimeToBus2.filter(item => item.from === 'Палех' && item.to === 'Иваново')[0]
+  const filtertime = GetDateToBus (RealTime(), bus.times)
+
+  return (<div>
+    1. Ближайший рейс через {TimeCalc(RealTime(), filtertime[0])} минут в {filtertime[0]} <br></br>
+    2. Рейс через {TimeCalc(RealTime(), filtertime[1])} минут в {filtertime[1]}<br></br>
+    3. Рейс через {TimeCalc(RealTime(), filtertime[2])} минут в {filtertime[2]}<br></br>
+  </div>
   )
 }
