188e7fea453e85576dbbf17ac1a0dcfba4aa06f4
DgurJIupyf <Dgurjiupyff@mail.ru>
2019-07-13 01:31:25 +0300

    Add server

--- app/src/App.js
+++ app/src/App.js
@@ -10,7 +10,7 @@ export function App() {
   const [json, setJson] = useState();
 
   if (!json) {
-    fetch("/db.json")
+    fetch("http://localhost:3000/")
       .then(res => res.json())
       .then(data => {
         setJson(data);
